6a28e7c627ca7f5f08d7e8578bdde923a922c569
CamiloMR <camilo98.2254@gmail.com>
2021-03-07 13:22:11 -0500

    Carga inicial pet book

---
+++ src/app/image.service.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ImageService } from './image.service';
+
+describe('ImageService', () => {
+  let service: ImageService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ImageService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
+
+describe('ImageService', () => {
+  let service: ImageService;
+
+  beforeEach(() => {
+    service = new ImageService();
+  });
+
+  it('Debe crear el servicio', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('getImages', () => {
+    it('Debe traer todas las imagenes y deben ser 5', () => {
+      const resp = service.getImages();
+      expect(resp.length).toBe(5);
+    });
+  });
+
+  describe('getImage(x)', () => {
+    it('Debe traer una imagen cuando el id existe en las imagenes', () => {
+      const img = service.getImage(2);
+      expect(img.id).toBe(2);
+      expect(img.brand).toBe('perro');
+      expect(img.url).toBe('assets/images/perro2.jpg');
+    });
+  });
+});
